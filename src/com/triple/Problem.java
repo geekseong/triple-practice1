7b2abff496ddcc9a47e67cf5c4aeec000d225ab5
geekseong <geekseong@gmail.com>
2021-09-04 11:00:50 +0900

    feat: 일정추천 알고리즘 구현 - 테이블을 순회 하는 방식으로 구현

--- src/com/triple/Problem.java
+++ src/com/triple/Problem.java
@@ -19,30 +19,59 @@ public class Problem {
         }
     }
     public int solve(){
-        return search(this.N - 1, this.M);
+//        return solution1(this.N - 1, this.M);
+        return solution2(this.N - 1, this.M);
     }
 
     // n번째 장소까지 확인했을때, 남은 시간이 leftTime일 경우의 만족도.
-    private int search(int n, int leftTime){
+    private int solution1(final int n, final int leftTime){
         if( n < 0 )
             return 0;
 
         // 만약 이전에 계산한 값인경우 저장한 값을 재사용.
-        if (scoreTable[n][leftTime] != INF) {
-            return scoreTable[n][leftTime];
+        if (this.scoreTable[n][leftTime] != INF) {
+            return this.scoreTable[n][leftTime];
         }
 
         // n번째 장소를 선택
-        final Place nowPlace = places.get(n);
+        final Place nowPlace = this.places.get(n);
 
         // n번째 장소를 선택했을떄 주어진 시간을 초과 하는 경우 n번째 선택하지 않고 탐색.
         if (leftTime - nowPlace.getSpendTime() < 0) {
-            return scoreTable[n][leftTime] = search(n - 1, leftTime);
+            return this.scoreTable[n][leftTime] = solution1(n - 1, leftTime);
         }
 
         // n번째 장소를 선택했을떄 주어진 시간을 초과 하지 않은 경우 n번째를 선택하고 탐색.
-        int choose = search(n - 1, leftTime - nowPlace.getSpendTime()) + nowPlace.getSatisfactionScore();
-        int notChoose = search(n-1, leftTime);
-        return scoreTable[n][leftTime] = Math.max(choose, notChoose);
+        int choose = solution1(n - 1, leftTime - nowPlace.getSpendTime()) + nowPlace.getSatisfactionScore();
+        int notChoose = solution1(n-1, leftTime);
+        return this.scoreTable[n][leftTime] = Math.max(choose, notChoose);
+    }
+
+    // n번째 장소까지 확인했을때, 남은 시간이 leftTime일 경우의 만족도.
+    private int solution2(final int n, final int leftTime) {
+
+        for (int i = 0; i <= n; i++) {
+
+            // n번째 장소를 선택
+            final Place nowPlace = this.places.get(i);
+
+            for (int j = 0; j <= leftTime; j++) {
+
+                // n번째 장소를 선택했을떄 주어진 시간을 초과 하는 경우 n번째 선택하지 않고 탐색.
+                if (j - nowPlace.getSpendTime() < 0) {
+                    this.scoreTable[i][j] = i - 1 < 0 ? 0 : this.scoreTable[i - 1][j];
+                }
+
+                // n번째 장소를 선택했을떄 주어진 시간을 초과 하지 않은 경우 n번째를 선택하고 탐색.
+                else {
+                    int nowScore = nowPlace.getSatisfactionScore();
+                    int choose = i - 1 < 0 ? nowScore : this.scoreTable[i - 1][j - nowPlace.getSpendTime()] + nowScore;
+                    int notChoose = i - 1 < 0 ? 0 : this.scoreTable[i - 1][j];
+                    this.scoreTable[i][j] = Math.max(choose, notChoose);
+                }
+            }
+        }
+
+        return this.scoreTable[n][leftTime];
     }
 }
